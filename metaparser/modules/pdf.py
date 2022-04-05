231c76c5709815c58b8a0b40b0c32caca1915e58
mwlodarc <m.wlodarczyk@stud.elka.pw.edu.pl>
2022-04-05 17:52:56 +0200

    add pdf parser

---
+++ metaparser/modules/pdf.py
@@ -0,0 +1,63 @@
+from typing import List, Dict, Any
+
+from PyPDF2 import PdfFileReader, PdfFileMerger
+
+from .base import BaseParser
+
+FIELD_TITLE = "/Title"
+FIELD_AUTHOR = "/Author"
+FIELD_SUBJECT = "/Subject"
+FIELD_KEYWORD = "/Keyword"
+FIELD_CREATED_DATE = "/Created Date"
+FIELD_MODIFIED_DATE = "/Modified Date"
+FIELD_CREATOR = "/Creator"
+FIELD_PRODUCER = "/Producer"
+FIELD_TRAPPED = "/Trapped"
+FIELD_DADURA = "/Dadura"
+
+
+class PDFParser(BaseParser):
+    def get_fields(self) -> List[str]:
+        return [
+            FIELD_TITLE,
+            FIELD_AUTHOR,
+            FIELD_SUBJECT,
+            FIELD_KEYWORD,
+            FIELD_CREATED_DATE,
+            FIELD_MODIFIED_DATE,
+            FIELD_CREATOR,
+            FIELD_PRODUCER,
+            FIELD_TRAPPED,
+            FIELD_DADURA
+        ]
+
+    @staticmethod
+    def supported_mimes() -> List[str]:
+        return ["application/pdf", "application/pdf-x"]
+
+    def __init__(self) -> None:
+        super().__init__()
+        metadata = None
+        filename = None
+
+    def parse(self, filename: str) -> None:
+        self.filename = filename
+        metadata = PdfFileReader(filename).getDocumentInfo()
+        self.metadata = {}
+        for field in metadata:
+            self.metadata[field] = metadata[field]
+
+    def set_field(self, field: str, value: Any) -> None:
+        if field not in self.get_fields():
+            raise KeyError("Field not present in parser")
+        self.metadata[field] = value
+        self.write()
+
+    def write(self) -> None:
+        pdf_merger = PdfFileMerger()
+        pdf_merger.append(self.filename)
+        pdf_merger.addMetadata(self.metadata)
+        pdf_merger.write(self.filename)
+
+    def get_all_values(self) -> Dict[str, str]:
+        return self.metadata
